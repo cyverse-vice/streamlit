5ef88ddccd339b892114608897085583a2c8dd92
Tyson L. Swetnam <tyson-swetnam@users.noreply.github.com>
2024-07-09 22:14:54 +0000

    created croissant w/ cors

---
+++ croissant/entry.sh
@@ -0,0 +1,19 @@
+while getopts "p:" opt; do
+  case ${opt} in
+    p)
+      streamlit_path=$OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+echo "$streamlit_path"
+
+exec streamlit run app.py $streamlit_path --server.enableCORS false --server.enableXsrfProtection false
